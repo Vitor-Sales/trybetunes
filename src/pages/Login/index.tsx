f734ad5ce21a7d342a50d07f7a38098124b4754d
Vitor Sales <vitormalisa@gmail.com>
2023-12-07 13:26:19 -0300

    Feat: Task 01 done. Created Login Input and Button

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,38 @@
+import { useState } from 'react';
+import { createUser } from '../../services/userAPI';
+
 function Login() {
+  const [loginValue, setLoginValue] = useState<string>('');
+  const [loading, setLoading] = useState(false);
+
+  const handleLoginClick = () => {
+    setLoading(true);
+    createUser({ name: loginValue })
+      .then(() => setLoading(false));
+  };
+
   return (
-    <h1>Login</h1>
+    <>
+      <h1>Login</h1>
+      <label htmlFor="login">Login</label>
+      <input
+        type="text"
+        id="login"
+        data-testid="login-name-input"
+        value={ loginValue }
+        onChange={ (e) => setLoginValue(e.target.value) }
+      />
+      <button
+        data-testid="login-submit-button"
+        disabled={ loginValue.length < 3 }
+        onClick={ handleLoginClick }
+      >
+        Entrar
+
+      </button>
+      {loading && <p>Carregando...</p>}
+    </>
+
   );
 }
 
