6012dc7aa2dfabf6cc00d632f9b6209ea918e287
Vitor Sales <vitormalisa@gmail.com>
2023-12-07 12:09:41 -0300

    Feat: Routes created.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Album from './pages/Album';
+import Search from './pages/Search';
+import NotFound from './pages/NotFound/NotFound';
+import Layout from './pages/Layout';
+
 function App() {
   return (
-    <p>Trybetunes</p>
+    <Routes>
+      <Route path="/" element={ <Layout /> }>
+        <Route path="/" element={ <Login /> } />
+        <Route path="/search" element={ <Search /> } />
+        <Route path="/album/:id" element={ <Album /> } />
+      </Route>
+      <Route path="/*" element={ <NotFound /> } />
+    </Routes>
   );
 }
 
