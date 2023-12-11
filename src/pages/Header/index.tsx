198ad8458ceedc584b42801a825880b94f280c02
Vitor Sales <vitormalisa@gmail.com>
2023-12-11 14:07:21 -0300

    Feat: Task 04 done. Passing through tests, but need to correct userName type

--- src/pages/Header/index.tsx
+++ src/pages/Header/index.tsx
@@ -1,13 +1,35 @@
 import { NavLink } from 'react-router-dom';
 import './header.css';
+import { useEffect, useState } from 'react';
+import { getUser } from '../../services/userAPI';
+import { UserType } from '../../types';
 
 function Header() {
+  const [loading, setLoading] = useState(true);
+  const [userName, setUserName] = useState<UserType | undefined>();
+  const showUser = async () => {
+    setLoading(true);
+    const name = await getUser();
+    setUserName(name);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    showUser();
+  }, []);
+
   return (
-    <header className="navbar" data-testid="header-component">
-      <NavLink to="/search">Search</NavLink>
-      <NavLink to="/favorites">Favorites</NavLink>
-      <NavLink to="profile">Profile</NavLink>
-    </header>
+    <>
+      {loading && <p>Carregando...</p>}
+      {!loading && (
+        <header className="navbar" data-testid="header-component">
+          <NavLink data-testid="link-to-search" to="/search">Search</NavLink>
+          <NavLink data-testid="link-to-favorites" to="/favorites">Favorites</NavLink>
+          <NavLink data-testid="link-to-profile" to="/profile">Profile</NavLink>
+          <p data-testid="header-user-name">{userName.name}</p>
+        </header>
+      )}
+    </>
   );
 }
 
