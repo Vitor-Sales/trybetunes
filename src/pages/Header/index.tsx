d1008dde8f264ee38e349ef16e4fae250fca7ec0
Vitor Sales <vitormalisa@gmail.com>
2023-12-11 16:31:08 -0300

    Fear: Task 04: type correction.

--- src/pages/Header/index.tsx
+++ src/pages/Header/index.tsx
@@ -2,14 +2,13 @@ import { NavLink } from 'react-router-dom';
 import './header.css';
 import { useEffect, useState } from 'react';
 import { getUser } from '../../services/userAPI';
-import { UserType } from '../../types';
 
 function Header() {
   const [loading, setLoading] = useState(true);
-  const [userName, setUserName] = useState<UserType | undefined>();
+  const [userName, setUserName] = useState<string>('');
   const showUser = async () => {
     setLoading(true);
-    const name = await getUser();
+    const { name } = await getUser();
     setUserName(name);
     setLoading(false);
   };
@@ -26,7 +25,7 @@ function Header() {
           <NavLink data-testid="link-to-search" to="/search">Search</NavLink>
           <NavLink data-testid="link-to-favorites" to="/favorites">Favorites</NavLink>
           <NavLink data-testid="link-to-profile" to="/profile">Profile</NavLink>
-          <p data-testid="header-user-name">{userName.name}</p>
+          <p data-testid="header-user-name">{userName}</p>
         </header>
       )}
     </>
